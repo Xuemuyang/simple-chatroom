902b8052f5755e87412924b69551babbcc179253
myoungxue <myoungxue@gmail.com>
2021-07-09 22:49:15 +0800

    feat: 通过 create-react-app 生成前端项目

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/index.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      teemo
+    </div>
+  );
+}
+
+export default App;
